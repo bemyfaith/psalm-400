13561e97d9246e7865a305c8c3a10d2b23afc219
xu <chasexu@126.com>
2026-04-21 03:13:04 +0800

    Update psalm layout and hymn set

--- psalm-data.js
+++ psalm-data.js
@@ -3,7 +3,7 @@ window.PSALM_DATA = [
     "number": 1,
     "title": "圣哉三一",
     "mp3": "assets/mp3/1 圣哉三一.mp3",
-    "score": ""
+    "score": "assets/scores/1 圣哉三一歌.jpg"
   },
   {
     "number": 2,
@@ -21,7 +21,7 @@ window.PSALM_DATA = [
     "number": 4,
     "title": "赞我天父",
     "mp3": "assets/mp3/4 赞我天父.mp3",
-    "score": ""
+    "score": "assets/scores/4 赞我天父歌.jpg"
   },
   {
     "number": 5,
@@ -2398,5 +2398,257 @@ window.PSALM_DATA = [
     "title": "阿们颂",
     "mp3": "assets/mp3/400 阿们颂.mp3",
     "score": "assets/scores/400 阿们颂.jpg"
+  },
+  {
+    "number": "f1",
+    "title": "神爱世人",
+    "mp3": "assets/mp3/f1 神爱世人.mp3",
+    "score": "assets/scores/f1 神爱世人.jpg"
+  },
+  {
+    "number": "f2",
+    "title": "对主你如何",
+    "mp3": "assets/mp3/f2 对主你如何.mp3",
+    "score": "assets/scores/f2 对主你如何.jpg"
+  },
+  {
+    "number": "f3",
+    "title": "爱何等大恩何等深",
+    "mp3": "assets/mp3/f3 爱何等大恩何等深.mp3",
+    "score": "assets/scores/f3 爱何等大恩何等深.jpg"
+  },
+  {
+    "number": "f4",
+    "title": "仰望十架",
+    "mp3": "assets/mp3/f4 仰望十架.mp3",
+    "score": "assets/scores/f4 仰望十架.jpg"
+  },
+  {
+    "number": "f5",
+    "title": "我要向山举目",
+    "mp3": "assets/mp3/f5 我要向山举目.mp3",
+    "score": "assets/scores/f5 我要向山举目.jpg"
+  },
+  {
+    "number": "f6",
+    "title": "哈利路亚赞美耶稣",
+    "mp3": "assets/mp3/f6 哈利路亚赞美耶稣.mp3",
+    "score": "assets/scores/f6 哈利路亚赞美耶稣.jpg"
+  },
+  {
+    "number": "f7",
+    "title": "可爱美名",
+    "mp3": "assets/mp3/f7 可爱美名.mp3",
+    "score": "assets/scores/f7 可爱美名.jpg"
+  },
+  {
+    "number": "f8",
+    "title": "耶稣做朋友",
+    "mp3": "assets/mp3/f8 耶稣做朋友.mp3",
+    "score": "assets/scores/f8 耶稣做朋友.jpg"
+  },
+  {
+    "number": "f9",
+    "title": "活时他爱我",
+    "mp3": "assets/mp3/f9 活时他爱我.mp3",
+    "score": "assets/scores/f9 活时他爱我.jpg"
+  },
+  {
+    "number": "f10",
+    "title": "广广似洋无边",
+    "mp3": "assets/mp3/f10 广广似洋无边.mp3",
+    "score": "assets/scores/f10 广广似洋无边.jpg"
+  },
+  {
+    "number": "f11",
+    "title": "主言永存",
+    "mp3": "assets/mp3/f11 主言永存.mp3",
+    "score": "assets/scores/f11 主言永存.jpg"
+  },
+  {
+    "number": "f12",
+    "title": "求主扶助",
+    "mp3": "assets/mp3/f12 求主扶助.mp3",
+    "score": "assets/scores/f12 求主扶助.jpg"
+  },
+  {
+    "number": "f13",
+    "title": "赐下真光",
+    "mp3": "assets/mp3/f13 赐下真光.mp3",
+    "score": "assets/scores/f13 赐下真光.jpg"
+  },
+  {
+    "number": "f14",
+    "title": "接受我心",
+    "mp3": "assets/mp3/f14 接受我心.mp3",
+    "score": "assets/scores/f14 接受我心.jpg"
+  },
+  {
+    "number": "f15",
+    "title": "进我的心",
+    "mp3": "assets/mp3/f15 进我的心.mp3",
+    "score": "assets/scores/f15 进我的心.jpg"
+  },
+  {
+    "number": "f16",
+    "title": "神啊求你鉴察我",
+    "mp3": "assets/mp3/f16 神啊求你鉴察我.mp3",
+    "score": "assets/scores/f16 神啊求你鉴察我.jpg"
+  },
+  {
+    "number": "f17",
+    "title": "我当完全属主",
+    "mp3": "assets/mp3/f17 我当完全属主.mp3",
+    "score": "assets/scores/f17 我当完全属主.jpg"
+  },
+  {
+    "number": "f18",
+    "title": "今我有快乐",
+    "mp3": "assets/mp3/f18 今我有快乐.mp3",
+    "score": "assets/scores/f18 今我有快乐.jpg"
+  },
+  {
+    "number": "f19",
+    "title": "喜乐满我心",
+    "mp3": "assets/mp3/f19 喜乐满我心.mp3",
+    "score": "assets/scores/f19 喜乐满我心.jpg"
+  },
+  {
+    "number": "f20",
+    "title": "恩如雨沛降",
+    "mp3": "assets/mp3/f20 恩如雨沛降.mp3",
+    "score": "assets/scores/f20 恩如雨沛降.jpg"
+  },
+  {
+    "number": "f21",
+    "title": "大山可以挪开",
+    "mp3": "assets/mp3/f21 大山可以挪开.mp3",
+    "score": "assets/scores/f21 大山可以挪开.jpg"
+  },
+  {
+    "number": "f22",
+    "title": "他顾念你们",
+    "mp3": "assets/mp3/f22 他顾念你们.mp3",
+    "score": "assets/scores/f22 他顾念你们.jpg"
+  },
+  {
+    "number": "f23",
+    "title": "主内有真平安",
+    "mp3": "assets/mp3/f23 主内有真平安.mp3",
+    "score": "assets/scores/f23 主内有真平安.jpg"
+  },
+  {
+    "number": "f24",
+    "title": "一宿哭泣早晨欢呼",
+    "mp3": "assets/mp3/f24 一宿哭泣早晨欢呼.mp3",
+    "score": "assets/scores/f24 一宿哭泣早晨欢呼.jpg"
+  },
+  {
+    "number": "f25",
+    "title": "压伤的芦苇他不折断",
+    "mp3": "assets/mp3/f25 压伤的芦苇他不折断.mp3",
+    "score": "assets/scores/f25 压伤的芦苇他不折断.jpg"
+  },
+  {
+    "number": "f26",
+    "title": "主我爱你",
+    "mp3": "assets/mp3/f26 主我爱你.mp3",
+    "score": "assets/scores/f26 主我爱你.jpg"
+  },
+  {
+    "number": "f27",
+    "title": "我真是爱耶稣",
+    "mp3": "assets/mp3/f27 我真是爱耶稣.mp3",
+    "score": "assets/scores/f27 我真是爱耶稣.jpg"
+  },
+  {
+    "number": "f28",
+    "title": "当与蒙召的恩相称",
+    "mp3": "assets/mp3/f28 当与蒙召的恩相称.mp3",
+    "score": "assets/scores/f28 当与蒙召的恩相称.jpg"
+  },
+  {
+    "number": "f29",
+    "title": "章显主荣美",
+    "mp3": "assets/mp3/f29 章显主荣美.mp3",
+    "score": "assets/scores/f29 章显主荣美.jpg"
+  },
+  {
+    "number": "f30",
+    "title": "众人以为美的事",
+    "mp3": "assets/mp3/f30 众人以为美的事.mp3",
+    "score": "assets/scores/f30 众人以为美的事.jpg"
+  },
+  {
+    "number": "f31",
+    "title": "要为主发光",
+    "mp3": "assets/mp3/f31 要为主发光.mp3",
+    "score": "assets/scores/f31 要为主发光.jpg"
+  },
+  {
+    "number": "f32",
+    "title": "忘记背后努力面前",
+    "mp3": "assets/mp3/f32 忘记背后努力面前.mp3",
+    "score": "assets/scores/f32 忘记背后努力面前.jpg"
+  },
+  {
+    "number": "f33",
+    "title": "分别是非",
+    "mp3": "assets/mp3/f33 分别是非.mp3",
+    "score": "assets/scores/f33 分别是非.jpg"
+  },
+  {
+    "number": "f34",
+    "title": "不要作糊涂人",
+    "mp3": "assets/mp3/f34 不要作糊涂人.mp3",
+    "score": "assets/scores/f34 不要作糊涂人.jpg"
+  },
+  {
+    "number": "f35",
+    "title": "务要叫邻舍喜悦",
+    "mp3": "assets/mp3/f35 务要叫邻舍喜悦.mp3",
+    "score": "assets/scores/f35 务要叫邻舍喜悦.jpg"
+  },
+  {
+    "number": "f36",
+    "title": "施比受更为有福",
+    "mp3": "assets/mp3/f36 施比受更为有福.mp3",
+    "score": "assets/scores/f36 施比受更为有福.jpg"
+  },
+  {
+    "number": "f37",
+    "title": "信心加上德行",
+    "mp3": "assets/mp3/f37 信心加上德行.mp3",
+    "score": "assets/scores/f37 信心加上德行.jpg"
+  },
+  {
+    "number": "f38",
+    "title": "手懒的要受贫穷",
+    "mp3": "assets/mp3/f38 手懒的要受贫穷.mp3",
+    "score": "assets/scores/f38 手懒的要受贫穷.jpg"
+  },
+  {
+    "number": "f39",
+    "title": "父在我们中间",
+    "mp3": "assets/mp3/f39 父在我们中间.mp3",
+    "score": "assets/scores/f39 父在我们中间.jpg"
+  },
+  {
+    "number": "f40",
+    "title": "谢饭歌",
+    "mp3": "assets/mp3/f40 谢饭歌.mp3",
+    "score": "assets/scores/f40 谢饭歌.jpg"
+  },
+  {
+    "number": "f41",
+    "title": "旧约目录歌",
+    "mp3": "assets/mp3/f41 旧约目录歌.mp3",
+    "score": "assets/scores/f41 旧约目录歌.jpg"
+  },
+  {
+    "number": "f42",
+    "title": "新约目录歌",
+    "mp3": "assets/mp3/f42 新约目录歌.mp3",
+    "score": "assets/scores/f42 新约目录歌.jpg"
   }
 ];
